20ac1c40021774693acf12a37c6da3720637d58a
DangNH1652005 <nguyenhaidang1652005@gmail.com>
2026-05-19 14:47:35 +0700

    initial frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <div>
+          This is button
+        </div>
+        <button className="btn btn-active btn-neutral">
+          Hello DaisyUI
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
