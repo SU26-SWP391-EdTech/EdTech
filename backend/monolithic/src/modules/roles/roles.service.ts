659c56ccbd94fb81206bf57e02df601a7674eee7
DangNH1652005 <nguyenhaidang1652005@gmail.com>
2026-05-28 21:15:34 +0700

    implement auto insert role into database

--- backend/monolithic/src/modules/roles/roles.service.ts
+++ backend/monolithic/src/modules/roles/roles.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { RolesRepository } from './roles.repository';
+import { RoleEnum } from 'src/common/enums/role.enum';
 
 @Injectable()
-export class RolesService {}
+export class RolesService implements OnModuleInit {
+  constructor(private readonly rolesRepository: RolesRepository) {}
+
+  public async onModuleInit() {
+    const roles = Object.values(RoleEnum);
+    await this.rolesRepository.seedRoles(roles);
+  }
+}
